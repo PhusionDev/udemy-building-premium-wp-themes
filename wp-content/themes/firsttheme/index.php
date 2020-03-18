96fa4847b798a2e63b775b110fd35bdf34d138bf
jaredhodgkins <jared.hodgkins@gmail.com>
2020-03-18 08:11:08 -0400

    Coding the Loop

--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -13,7 +13,12 @@
       </a>
       By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php echo get_the_author(); ?></a>
     </div>
+    <div>
+      <?php the_excerpt(); ?>
+    </div>
+    <a href="<?php echo get_the_permalink(); ?>" title="<?php the_title_attribute(); ?>">Read More <span class="u-screen-reader-text">About <?php the_title(); ?></span></a>
   <?php } ?>
+  <?php the_posts_pagination(); ?>
 <?php } else { ?>
   <p>Sorry, no posts matched your criteria.</p>
 <?php } ?>
