4aa4f33fb2b9b386b55236a3cce384e2d4b6561b
jaredhodgkins <jared.hodgkins@gmail.com>
2020-03-19 14:09:42 -0400

    Compressing Images Copying Files

--- wp-content/themes/firsttheme/gulpfile.babel.js
+++ wp-content/themes/firsttheme/gulpfile.babel.js
@@ -19,21 +19,42 @@ import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
 
 const PRODUCTION = yargs.argv.prod;
 const paths = {
   styles: {
     src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
     dest: 'dist/assets/css'
+  },
+  images: {
+    src: 'src/assets/images/**/*.{jpg,jpeg,png,bmp,svg,gif}',
+    dest: 'dist/assets/images'
+  },
+  other: {
+    src: ['src/assets/**/*', '!src/assets/{images,js,scss}/**/*'],
+    dest: 'dist/assets'
   }
 }
-export const styles = (done) => {
+
+export const styles = () => {
   return gulp.src(paths.styles.src)
     .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
     .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
     .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const images = () => {
+  return gulp.src(paths.images.src)
+    .pipe(gulpif(PRODUCTION, imagemin()))
+    .pipe(gulp.dest(paths.images.dest));
+}
+
+export const copy = () => {
+  return gulp.src(paths.other.src)
+    .pipe(gulp.dest(paths.other.dest));
 }
 
 export const watch = () => {
