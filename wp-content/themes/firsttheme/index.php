d0c9b5d3559c29e9efb1146db095c74fc2b5adf6
jaredhodgkins <jared.hodgkins@gmail.com>
2020-03-22 18:56:59 -0400

    Creating Sidebars Dynamically

--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -28,7 +28,7 @@
     </div>
 
     <?php if(is_active_sidebar( 'primary-sidebar' )) { ?>
-      <div class="o-row__column o-row__column--span-12 o-row__column--span-4@medium">
+      <div class="o-row__column o-row__column--span-4@medium">
         <?php get_sidebar(); ?>
       </div>
     <?php } ?>
