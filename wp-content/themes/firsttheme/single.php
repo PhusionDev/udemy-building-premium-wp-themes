bd18467895db363bff85284ac2ed4f43617be0d6
jaredhodgkins <jared.hodgkins@gmail.com>
2020-03-30 02:43:42 -0400

    Adding An Option To Show/Hide Author Info

--- wp-content/themes/firsttheme/single.php
+++ wp-content/themes/firsttheme/single.php
@@ -14,7 +14,11 @@
         <?php while (have_posts()) {?>
           <?php the_post(); ?>
           <?php get_template_part( 'template-parts/post/content' ); ?>
-          <?php get_template_part( 'template-parts/single/author' ); ?>
+          <?php
+            if(get_theme_mod( '_themename_display_author_info', true )) {
+              get_template_part( 'template-parts/single/author' );
+            }
+          ?>
 
         <?php } ?>
       <?php } else { ?>
